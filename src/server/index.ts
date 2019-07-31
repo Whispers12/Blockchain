7e65d5200c3611e286687e0d38db794ff42ea68b
Whispers12 <frolovandrey752@gmail.com>
2019-07-31 11:10:55 +0300

    feat: add mine block method in api

--- src/server/index.ts
+++ src/server/index.ts
@@ -9,6 +9,22 @@ const server: fastify.FastifyInstance<
 > = fastify({});
 const blockchain = new Blockchain();
 
-server.get("/api/blocks", (req, res) => {
-  res.json(blockchain.chain);
+server.get("/api/blocks", (_, res) => {
+  res.send(blockchain.chain);
+});
+
+server.post("/api/mine", (req, res) => {
+  const { data } = req.body;
+
+  blockchain.addBlock({ data });
+
+  res.redirect("/api/blocks");
+});
+
+server.listen(3000, (err, address) => {
+  if (err) {
+    server.log.error(err);
+    process.exit(1);
+  }
+  server.log.info(`server listening on ${address}`);
 });
