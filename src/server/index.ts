dcadba9dbab7399eab1970dd83cd649cac47d692
Whispers12 <frolovandrey752@gmail.com>
2019-08-02 19:59:46 +0300

    feat(server): add transaction pool map api method

--- src/server/index.ts
+++ src/server/index.ts
@@ -58,6 +58,10 @@ server.post("/api/transact", (req, res) => {
   }
 });
 
+server.get("/api/transaction-pool-map", (req, res) => {
+  res.send(transactionPool.getTransactionMap());
+});
+
 const syncChains = () => {
   request({ url: `${ROOT_NODE_ADDRESS}/api/blocks` }, (error, resp, body) => {
     if (!error && resp.statusCode === 200) {
